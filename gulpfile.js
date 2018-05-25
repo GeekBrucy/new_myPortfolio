aa8083f4b10851897af4c34d8f3805e368d5cef7
geekbrucy <peichunzhou1006@gmail.com>
2018-05-25 16:41:21 +1000

    init code, file structure

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ const sass        = require('gulp-sass');
 
 // Compile Sass & Inject Into Browser
 gulp.task('sass', function() {
-    return gulp.src(['src/scss/*.scss'])
+    return gulp.src(['src/scss/style.sass'])
         .pipe(sass())
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("src/css/"))
         .pipe(browserSync.stream());
 });
 
@@ -14,11 +14,11 @@ gulp.task('sass', function() {
 // Watch Sass & Serve
 gulp.task('serve', ['sass'], function() {
     browserSync.init({
-        server: "./src"  
+        server: "."  
     });
 
-    gulp.watch(['src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch(['src/scss/style.sass'], ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
 });
 
 // Default Task
